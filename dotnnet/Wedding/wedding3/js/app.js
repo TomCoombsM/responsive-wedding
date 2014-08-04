1faa49a95ef2f7564168c14f61e5466ba1352433
tomcoombs <tom.coombs@moonpig.com>
2014-08-04 13:24:25 +0100

    Add ng reg boiler plate

---
+++ dotnnet/Wedding/wedding3/js/app.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    var app = angular.module("reservations", ["ngRoute"] );
+
+    app.config(function ($routeProvider) {
+        $routeProvider.when("/main", {
+            templateUrl: "ng-views/main.html",
+            controller : "MainController"
+        })
+        .otherwise({redirectTo:"/main"})
+    });
+}());
