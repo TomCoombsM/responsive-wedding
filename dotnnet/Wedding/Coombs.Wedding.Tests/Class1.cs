9c4be897a772fc99028b8265e360a8edb1bf8857
tomcoombs <tom.coombs@moonpig.com>
2014-10-12 18:35:08 +0100

    add tests

---
+++ dotnnet/Wedding/Coombs.Wedding.Tests/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Coombs.Wedding.Tests
+{
+    public class Class1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(4, 2 + 2);
+        }
+    }
+}
