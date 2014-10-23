e58721a8010bc1f2c663d7ecc100414c2c68d009
Tom Coombs <email@tomcoombs.co.uk>
2014-10-23 13:47:34 +0100

    add deployment powershell

---
+++ dotnnet/Wedding/Coombs.Deploy/deploy.ps1
@@ -0,0 +1,14 @@
+﻿
+# Don't forget to change the path below to your copy of Octopus.Client.dll
+Add-Type -Path "C:\Users\tom.TOMFAB\Documents\responsive-wedding\dotnnet\Wedding\packages\Octopus.Client.2.5.11.610\lib\net40\Octopus.Client.dll"
+ 
+ 
+$endpoint = new-object Octopus.Client.OctopusServerEndpoint "https://goserver","API-RORM5DBGZMOSYGE7OSE5EZPQA"
+$repository = new-object Octopus.Client.OctopusRepository $endpoint
+ 
+$envId = $repository.Environments.FindByName("Production")
+$machines = $repository.Environments.GetMachines($envId)
+ 
+$machines | Format-Table
+
+
