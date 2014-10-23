df8d249361bbb8625ea12c289481d2722c2ea497
Tom Coombs <email@tomcoombs.co.uk>
2014-10-23 22:36:37 +0100

    push to Octo nuget

--- build.bat
+++ build.bat
@@ -1,1 +1,5 @@
 C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe /t:Rebuild /p:configuration=release;RunOctoPack=true /fileLogger /flp:logfile=buildlog.txt /flp1:logfile=builderrors.txt;errorsonly /flp2:logfile=buildwarnings.txt;warningsonly dotnnet\Wedding\wedding.sln
+
+
+
+dotnnet\Wedding\.nuget\NuGet.exe push "C:\Program Files (x86)\Go Agent\pipelines\Wedding\dotnnet\Wedding\wedding3\bin\wedding3.1.0.0.0.nupkg" -ApiKey API-WLVJX7Z98ZHLDAX4MGX9LTNDXG8 -Source http://goserver/nuget/packages
