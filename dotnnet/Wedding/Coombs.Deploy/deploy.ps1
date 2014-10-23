0d4fc9189fafd9e1f4514ebb655eaa3036bc17b3
Tom Coombs <email@tomcoombs.co.uk>
2014-10-23 20:50:56 +0100

    fix

--- dotnnet/Wedding/Coombs.Deploy/deploy.ps1
+++ dotnnet/Wedding/Coombs.Deploy/deploy.ps1
@@ -1,6 +1,9 @@
 ﻿
 # Don't forget to change the path below to your copy of Octopus.Client.dll
-Add-Type -Path "C:\Users\tom.TOMFAB\Documents\responsive-wedding\dotnnet\Wedding\packages\Octopus.Client.2.5.11.610\lib\net40\Octopus.Client.dll"
+Add-Type -Path "C:\Program Files (x86)\Go Agent\pipelines\Wedding\dotnnet\Wedding\packages\Octopus.Client.2.5.11.610\lib\net40\ReleaseOctopus.Client.dll"
+
+
+
  
  
 $endpoint = new-object Octopus.Client.OctopusServerEndpoint "https://goserver","API-RORM5DBGZMOSYGE7OSE5EZPQA"
